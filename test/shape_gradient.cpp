55698833202eb376baf4d07b8e9f1cceeca29519
Michaja Bösch <boeschm@student.ethz.ch>
2015-04-23 21:55:07 +0200

    gather-type gradient computation

--- test/shape_gradient.cpp
+++ test/shape_gradient.cpp
@@ -3,24 +3,33 @@
 #include <unordered_set>
 
 #include "waveblocks.hpp"
+#include "util/time.hpp"
 
 using namespace waveblocks;
 
 int main()
 {
-    const dim_t D = 5;
+    double start;
+    
+    const dim_t D = 6;
     typedef HyperCubicShape<D> Shape;
     
-    Shape shape(MultiIndex<D>{{10,10,10,10,10}});
+    Shape shape(MultiIndex<D>{{10,10,10,10,10,10}});
     
+    start = getRealTime();
     SlicedShapeEnumeration<D,Shape> enumeration(shape);
+    std::cout << "create enumeration: " << (getRealTime() - start) << std::endl;
+    
+    start = getRealTime();
     ShapeExtensionEnumeration<D,Shape> extension(shape);
+    std::cout << "create extension: " << (getRealTime() - start) << std::endl;
     
     HagedornParameterSet<D> parameters;
     
     std::vector<complex_t> coefficients(enumeration.size());
     
     //std::cout << "coefficients: " << std::endl;
+    start = getRealTime();
     {
         std::size_t ordinal = 0;
         for (auto index : enumeration) {
@@ -39,31 +48,66 @@ int main()
             ordinal++;
         }
     }
+    std::cout << "create coefficients: " << (getRealTime() - start) << std::endl;
     
     std::vector<complex_t> gradient[D];
-    
     Eigen::Matrix<real_t,D,1> x;
-    for (dim_t d = 0; d < D; d++)
-        evaluateWavepacketGradient(coefficients, parameters, enumeration, extension, x, d, gradient[d]);
     
-    if (false) {
-    std::size_t ordinal = 0;
-    {
-        for (auto index : enumeration) {
-            complex_t value = gradient[0][ordinal++];
-            std::cout << index << "  " << value.real() << " " << std::setw(10) << value.imag() << "\n";
-        }
-        
-        for (auto index : extension) {
-            complex_t value = gradient[0][ordinal++];
-            std::cout << index << "  " << std::setw(10) << value.real() << " " << std::setw(10) << value.imag() << "\n";
-        }
-    }
-    }
+    //scatter-type
+//     start = getRealTime();
+//     for (dim_t d = 0; d < D; d++)
+//         evaluateWavepacketGradient(coefficients, parameters, enumeration, extension, x, d, gradient[d]);
+//     std::cout << "evaluate gradient (scatter type): " << (getRealTime() - start) << std::endl;
+    
+    //gather-type
+    start = getRealTime();
+    GradientOperator<D,Shape> nabla;
+    for (dim_t d = 0; d < D; d++)
+        nabla.evaluateWavepacketGradient(coefficients, parameters, enumeration, extension, x, d, gradient[d]);
+    std::cout << "evaluate gradient (gather type): " << (getRealTime() - start) << std::endl;
+    
+//     std::size_t ordinal = 0;
+//     {
+//         for (auto index : enumeration) {
+//             std::cout << index << "  ";
+//             for (dim_t d = 0; d < D; d++) {
+//                 complex_t value = gradient[d][ordinal];
+//                 std::cout << std::setw(11) << value.real() << " ";
+//             }
+//             for (dim_t d = 0; d < D; d++) {
+//                 complex_t value = gradient[d][ordinal];
+//                 std::cout << std::setw(11) << value.imag() << " ";
+//             }
+//             std::cout << "\n";
+//             ++ordinal;
+//         }
+//         
+//         for (auto index : extension) {
+//             std::cout << index << "  ";
+//             for (dim_t d = 0; d < D; d++) {
+//                 complex_t value = gradient[d][ordinal];
+//                 std::cout << std::setw(11) << value.real() << " ";
+//             }
+//             for (dim_t d = 0; d < D; d++) {
+//                 complex_t value = gradient[d][ordinal];
+//                 std::cout << std::setw(11) << value.imag() << " ";
+//             }
+//             std::cout << "\n";
+//             ++ordinal;
+//         }
+//     }
+    
+    return 0;
     
     //compare result to csv file 
+    std::cout << "compare results to csv file {" << std::endl;
     std::ifstream csv("../../test/gradient.csv");
+    if (csv.fail())
+        std::cout << "   [ERROR] File not found!" << std::endl;
+    
+    std::size_t lines = 0;
     while (csv.good()) {
+        ++lines;
         real_t tol = 1e-10;
         
         Eigen::Matrix<complex_t,D,1> solution;
@@ -111,6 +155,8 @@ int main()
         if ( error > tol)
             std::cout << "   [FAILURE] gradient at node " << index << " does not match entry in csv file. error = " << error << std::endl;
     }
+    std::cout << "   [INFO] lines processed: " << lines << std::endl;
+    std::cout << "}" << std::endl;
     
     return 0;
 }
--- waveblocks/wavepacket_gradient.hpp
+++ waveblocks/wavepacket_gradient.hpp
@@ -15,99 +15,96 @@
 
 namespace waveblocks {
 
-/**
- * Computes gradient by gather-type stencil application.
- * See master thesis chapter 3.8 for details.
- */
 template<dim_t D, class S>
-void evaluateWavepacketGradientGatherType(const std::vector<complex_t> &coefficients, 
-                                const HagedornParameterSet<D> &parameters, 
-                                const SlicedShapeEnumeration<D,S> &enumeration,
-                                const ShapeExtensionEnumeration<D,S> &extension,
-                                const Eigen::Matrix<real_t,D,1> &x,
-                                dim_t axis,
-                                std::vector<complex_t> &result)
+class GradientOperator
 {
-    auto &eps = parameters.eps;
-    auto &P = parameters.P;
-    auto &p = parameters.p;
+public:
     
-    Eigen::Matrix<complex_t,D,D> Pbar = P * P.adjoint() * P.inverse().transpose();
-    
-    std::size_t shape_size = enumeration.size();
-    
-    result.clear();
-    result.resize(shape_size + extension.size());
-    
-    //iterate over each slice [i = index of current slice]
-    auto slices = enumeration.slices();
-    for (std::size_t i = 0; i < slices.count(); i++) {
-        //loop over all multi-indices within current slice [j = position of multi-index within current slice]
-        for (std::size_t j = 0; j < slices[i].size(); j++) {
-            MultiIndex<D> curr_index = slices[i][j];
-            std::size_t curr_ordinal = slices[i].offset() + j;
-            
-            //central node
-            complex_t cc = coefficients[curr_ordinal]*p(axis,0);
-            
+    /**
+    * Computes gradient by gather-type stencil application.
+    * See master thesis chapter 3.8 for details.
+    */
+    void evaluateWavepacketGradient(const std::vector<complex_t> &coefficients, 
+                                    const HagedornParameterSet<D> &parameters, 
+                                    const SlicedShapeEnumeration<D,S> &enumeration,
+                                    const ShapeExtensionEnumeration<D,S> &extension,
+                                    const Eigen::Matrix<real_t,D,1> &x,
+                                    dim_t axis,
+                                    std::vector<complex_t> &result)
+    {
+        auto &eps = parameters.eps;
+        auto &P = parameters.P;
+        auto &p = parameters.p;
+        
+        Eigen::Matrix<complex_t,D,D> Pbar = P * P.adjoint() * P.inverse().transpose();
+        
+        std::size_t shape_size = enumeration.size();
+        
+        result.clear();
+        result.resize(shape_size + extension.size());
+        
+        //iterate over each slice [i = index of current slice]
+        auto slices = enumeration.slices();
+        for (std::size_t i = 0; i < slices.count(); i++) {
+            //loop over all multi-indices within current slice [j = position of multi-index within current slice]
+            for (std::size_t j = 0; j < slices[i].size(); j++) {
+                MultiIndex<D> curr_index = slices[i][j];
+                std::size_t curr_ordinal = slices[i].offset() + j;
+                
+                //central node
+                complex_t cc = coefficients[curr_ordinal]*p(axis,0);
+                
+                //backward neighbours
+                Eigen::Matrix<complex_t,D,1> cb;
+                for (dim_t d = 0; d < D; d++) {
+                    if (curr_index[d] != 0) {
+                        MultiIndex<D> prev_index = curr_index; prev_index[d] -= 1;
+                        std::size_t prev_ordinal = slices[i-1].find(prev_index);
+                        assert (prev_ordinal < slices[i-1].size()); //assert that backward neighbour exist
+                        prev_ordinal += slices[i-1].offset();
+                        
+                        cb[d] = std::sqrt(real_t(curr_index[d])) * coefficients[prev_ordinal];
+                    }
+                }
+                
+                //forward neighbours
+                Eigen::Matrix<complex_t,D,1> cf;
+                for (dim_t d = 0; d < D; d++) {
+                    MultiIndex<D> next_index = curr_index; next_index[d] += 1;
+                    
+                    if (i+1 != slices.count()) {
+                        std::size_t next_ordinal = slices[i+1].find(next_index);
+                        if (next_ordinal < slices[i+1].size()) {
+                            next_ordinal += slices[i+1].offset();
+                            
+                            cf[d] = std::sqrt(real_t(curr_index[d]+1)) * coefficients[next_ordinal];
+                        }
+                    }
+                }
+                
+                result[curr_ordinal] = -(eps/std::sqrt(real_t(2))*(Pbar.row(axis).dot(cf) + P.row(axis).dot(cb)) + cc*p(axis,0));
+            }
+        }
+        
+        std::size_t curr_ordinal = enumeration.size();
+        for (auto curr_index : extension) {
             //backward neighbours
             Eigen::Matrix<complex_t,D,1> cb;
             for (dim_t d = 0; d < D; d++) {
                 if (curr_index[d] != 0) {
                     MultiIndex<D> prev_index = curr_index; prev_index[d] -= 1;
-                    std::size_t prev_ordinal = slices[i-1].find(prev_index);
-                    assert (prev_ordinal < slices[i-1].size()); //assert that backward neighbour exist
-                    prev_ordinal += slices[i-1].offset();
-                    
-                    cb[d] = std::sqrt(real_t(curr_index[d])) * coefficients[prev_ordinal];
+                    std::size_t prev_ordinal = enumeration.find(prev_index);
+                    if (prev_ordinal < enumeration.size())
+                        cb[d] = std::sqrt(real_t(prev_index[d] + 1))*coefficients[prev_ordinal];
                 }
             }
             
-            //forward neighbours
-            Eigen::Matrix<complex_t,D,1> cf;
-            for (dim_t d = 0; d < D; d++) {
-                MultiIndex<D> next_index = curr_index; next_index[d] += 1;
-                
-                bool is_ext = true;
-                std::size_t next_ordinal;
-                if (i+1 != slices.count()) {
-                    next_ordinal = slices[i+1].find(next_index);
-                    if (next_ordinal < slices[i+1].size()) {
-                        is_ext = false;
-                        next_ordinal += slices[i+1].offset();
-                    }
-                }
-                if (is_ext) {
-                    next_ordinal = extension.find(next_index);
-                    assert (next_ordinal < extension.size()); //assert that forward neighbour exists
-                    next_ordinal += enumeration.size();
-                }
-                
-                cf[d] = std::sqrt(real_t(curr_index[d]+1)) * coefficients[next_ordinal];
-            }
+            result[curr_ordinal] = -(eps/std::sqrt(real_t(2))*(P.row(axis).dot(cb)));
             
-            result[curr_ordinal] = eps/std::sqrt(real_t(2))*(Pbar.row(axis)*cf + P.row(axis)*cb) + cc*p(axis,0);
+            ++curr_ordinal;
         }
     }
-    
-    std::size_t curr_ordinal = enumeration.size();
-    for (auto curr_index : extension) {
-        //backward neighbours
-        Eigen::Matrix<complex_t,D,1> cb;
-        for (dim_t d = 0; d < D; d++) {
-            if (curr_index[d] != 0) {
-                MultiIndex<D> prev_index = curr_index; prev_index[d] -= 1;
-                std::size_t prev_ordinal = extension.find(prev_index);
-                if (prev_ordinal < extension.size())
-                    cb[d] = std::sqrt(real_t(prev_index[d] + 1))*coefficients[prev_ordinal];
-            }
-        }
-        
-        result[curr_ordinal] = eps/std::sqrt(real_t(2))*(P.row(axis)*cb);
-        
-        ++curr_ordinal;
-    }
-}
+};
 
 /**
  * Computes gradient by scatter-type stencil application.
