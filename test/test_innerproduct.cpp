d2925c14a5e10484dbc8e9a901525426f5f2b8c1
Benedek Vartok <vartokb@student.ethz.ch>
2015-06-26 16:48:01 +0200

    started on quadrature rule

--- test/test_innerproduct.cpp
+++ test/test_innerproduct.cpp
@@ -4,11 +4,13 @@
 #include <Eigen/Core>
 
 #include "waveblocks/basic_types.hpp"
+#include "waveblocks/gauss_hermite_qr.hpp"
 #include "waveblocks/hawp.hpp"
 #include "waveblocks/hawp_paramset.hpp"
 #include "waveblocks/homogeneous_inner_product.hpp"
 #include "waveblocks/shape_enumerator.hpp"
 #include "waveblocks/shape_hypercubic.hpp"
+#include "waveblocks/stdarray2stream.hpp"
 #include "waveblocks/tiny_multi_index.hpp"
 
 using namespace waveblocks;
@@ -20,18 +22,30 @@ int main()
     const dim_t order = 8;
     using MultiIndex = TinyMultiIndex<unsigned short, D>;
 
+    // Set up sample 1D wavepacket.
     ShapeEnumerator<D, MultiIndex> enumerator;
     ShapeEnum<D, MultiIndex> shape_enum =
         enumerator.generate(HyperCubicShape<D>(N));
     HaWpParamSet<D> param_set;
     std::cout << param_set << std::endl;
     std::vector<complex_t> coeffs(N, 0);
+    GaussHermiteQR qr(order);
+
+    // Print QR nodes and weights.
+    std::cout << "nodes: {";
+    for (auto x : qr.nodes) std::cout << " " << x;
+    std::cout << " }\n";
+
+    std::cout << "weights: {";
+    for (auto x : qr.weights) std::cout << " " << x;
+    std::cout << " }\n";
 
     HaWp<D, MultiIndex> packet(0.6, &param_set, &shape_enum, &coeffs);
 
-    HomogeneousInnerProduct<D, MultiIndex> ip;
+    // Calculate inner product matrix, print it.
+    HomogeneousInnerProduct<D, MultiIndex, GaussHermiteQR> ip;
     CMatrix<Eigen::Dynamic, Eigen::Dynamic> mat =
-        ip.build_matrix(packet, order);
+        ip.build_matrix(packet, qr);
 
     std::cout << "IP matrix:\n" << mat << std::endl;
 
