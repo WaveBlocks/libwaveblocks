4ff4d93babc5e36335508fbe63109889629a298b
Benedek Vartok <vartokb@student.ethz.ch>
2015-06-28 19:36:24 +0200

    made Gauss-Hermite table generation work

--- test/test_innerproduct.cpp
+++ test/test_innerproduct.cpp
@@ -11,6 +11,7 @@
 #include "waveblocks/shape_enumerator.hpp"
 #include "waveblocks/shape_hypercubic.hpp"
 #include "waveblocks/stdarray2stream.hpp"
+#include "waveblocks/tables_gausshermite.hpp"
 #include "waveblocks/tiny_multi_index.hpp"
 
 using namespace waveblocks;
@@ -33,11 +34,13 @@ int main()
 
     // Print QR nodes and weights.
     std::cout << "nodes: {";
-    for (auto x : qr.nodes) std::cout << " " << x;
+    //for (auto x : qr.nodes) std::cout << " " << x;
+    for (auto x : gauss_hermite_rules[order].nodes) std::cout << " " << x;
     std::cout << " }\n";
 
     std::cout << "weights: {";
-    for (auto x : qr.weights) std::cout << " " << x;
+    //for (auto x : qr.weights) std::cout << " " << x;
+    for (auto x : gauss_hermite_rules[order].weights) std::cout << " " << x;
     std::cout << " }\n";
 
     HaWp<D, MultiIndex> packet(0.6, &param_set, &shape_enum, &coeffs);
---
+++ waveblocks/quadrature_rule.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+#include "basic_types.hpp"
+
+namespace waveblocks {
+
+struct QuadratureRule
+{
+  const size_t order;
+  const std::vector<real_t> nodes;
+  const std::vector<real_t> weights;
+
+  QuadratureRule(const size_t order,
+                 const std::vector<real_t>& nodes,
+                 const std::vector<real_t>& weights)
+    : order(order), nodes(nodes), weights(weights)
+  {}
+};
+
+}
