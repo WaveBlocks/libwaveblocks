e35062076820438bc16f225f08255a7edab52016
Benedek Vartok <vartokb@student.ethz.ch>
2015-08-21 19:05:50 +0200

    made GaussHermiteQR usable as a template parameter

--- waveblocks/gauss_hermite_qr.hpp
+++ waveblocks/gauss_hermite_qr.hpp
@@ -15,12 +15,15 @@ template <dim_t ORDER>
 struct GaussHermiteQR
 {
     static const dim_t order = ORDER;
-    const std::vector<real_t> nodes, weights;
 
-    GaussHermiteQR()
-        : nodes(gauss_hermite_rules[ORDER-1].nodes)
-        , weights(gauss_hermite_rules[ORDER-1].weights)
+    static const std::vector<real_t>& nodes()
     {
+        return gauss_hermite_rules[ORDER-1].nodes;
+    }
+
+    static const std::vector<real_t>& weights()
+    {
+        return gauss_hermite_rules[ORDER-1].weights;
     }
 };
 
