2d37742340c78f88cf7d6df12bde0e0ed88887ce
Benedek Vartok <vartokb@student.ethz.ch>
2015-08-28 20:04:05 +0200

    adjusted IP to work with multidimensional QR

--- waveblocks/homogeneous_inner_product.hpp
+++ waveblocks/homogeneous_inner_product.hpp
@@ -20,9 +20,9 @@ class HomogeneousInnerProduct
 {
 public:
     // TODO: make D-dimensional, replace Dynamic
-    using CMatrixDD = CMatrix<Eigen::Dynamic, Eigen::Dynamic>;
-    using CMatrix1D = CMatrix<1, Eigen::Dynamic>;
-    using RMatrix1D = RMatrix<1, Eigen::Dynamic>;
+    using CMatrixNN = CMatrix<Eigen::Dynamic, Eigen::Dynamic>;
+    using CMatrix1N = CMatrix<1, Eigen::Dynamic>;
+    using CMatrixDN = CMatrix<D, Eigen::Dynamic>;
     using NodeMatrix = typename QR::NodeMatrix;
     using WeightVector = typename QR::WeightVector;
 
@@ -30,45 +30,48 @@ public:
     {
     }
 
-    CMatrixDD build_matrix(const HaWp<D, MultiIndex>& packet)
+    CMatrixNN build_matrix(const HaWp<D, MultiIndex>& packet)
         const
     {
-        const dim_t order = QR::order;
+        const dim_t n_nodes = QR::number_nodes();
         const real_t eps = packet.basis.eps;
         const CMatrix<D,1>& q = complex_t(1, 0) * packet.basis.parameters->q;
         const CMatrix<D,D>& Q = packet.basis.parameters->Q;
         NodeMatrix nodes;
         WeightVector weights;
         std::tie(nodes, weights) = QR::nodes_and_weights();
-        const CMatrix1D cnodes = complex_t(1, 0) * nodes;
-        const CMatrix1D cweights = complex_t(1, 0) * weights;
+        const CMatrixDN cnodes = complex_t(1, 0) * nodes;
+        const CMatrix1N cweights = complex_t(1, 0) * weights;
 
         // Compute affine transformation.
         auto Qs = (Q * Q.adjoint()).inverse().sqrt().inverse();
 
         // Transform nodes.
-        CMatrix1D transformed_nodes =
-            q.replicate(1, order) + eps * (Qs * cnodes);
+        CMatrixDN transformed_nodes =
+            q.replicate(1, n_nodes) + eps * (Qs * cnodes);
 
         // TODO: Apply operator.
-        CMatrix1D values = CMatrix1D::Ones(1, order);
+        CMatrix1N values = CMatrix1N::Ones(1, n_nodes);
 
         Eigen::Array<complex_t, 1, Eigen::Dynamic>
             factor = std::pow(eps, D) * cweights.array() * values.array();
-        std::cout << "factor: " << factor << std::endl;
+        //std::cout << "factor: " << factor << std::endl;
 
         HaWpBasisVector<Eigen::Dynamic> bases = packet.basis.
             at(transformed_nodes).all();
-        std::cout << "bases:\n" << bases << std::endl;
+        //std::cout << "bases(:,0):\n" << bases.col(0) << std::endl;
+
+        //std::cout << "factor: " << factor.rows() << " x " << factor.cols() << "\n";
+        //std::cout << "bases: " << bases.rows() << " x " << bases.cols() << "\n";
 
         // Build matrix.
         const dim_t N = bases.rows();
-        CMatrixDD result = CMatrixDD::Zero(N, N);
+        CMatrixNN result = CMatrixNN::Zero(N, N);
         for(dim_t i = 0; i < N; ++i)
         {
             for(dim_t j = 0; j < N; ++j)
             {
-                for (size_t k = 0; k < order; ++k)
+                for(dim_t k = 0; k < n_nodes; ++k)
                 {
                     result(i, j) += factor(k) * conj(bases(i, k)) * bases(j, k);
                 }
