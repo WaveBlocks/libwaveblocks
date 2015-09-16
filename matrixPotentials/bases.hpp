a704565a7236b16cd8e800f91aa264c3db275eb1
lmis <lionel.miserez@gess.ethz.ch>
2015-09-16 22:48:11 +0200

    Put everything in namespaces; Made spezializations;

---
+++ matrixPotentials/bases.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#define IMPORT_TYPES_FROM(B,N,D)  \
+  using scalar_type = typename B<N,D>::scalar_type;                        \
+  using potential_type = typename B<N,D>::potential_type;                       \
+  using jacobian_type = typename B<N,D>::jacobian_type;                         \
+  using hessian_type = typename B<N,D>::hessian_type;                           \
+  using local_quadratic_type = typename B<N,D>::local_quadratic_type;           \
+  using potential_evaluation_type = typename B<N,D>::potential_evaluation_type; \
+  using jacobian_evaluation_type = typename B<N,D>::jacobian_evaluation_type;   \
+  using hessian_evaluation_type = typename B<N,D>::hessian_evaluation_type;     \
+  using potential_return_type = typename B<N,D>::potential_return_type;         \
+  using jacobian_return_type = typename B<N,D>::jacobian_return_type;           \
+  using hessian_return_type = typename B<N,D>::hessian_return_type              \
+  
+namespace matrixPotentials {
+  namespace bases {
+    template <int N, int D>
+    struct Basis;
+
+    template <int N, int D>
+    using Canonical = typename Basis<N,D>::Canonical;
+
+    template< int N, int D>
+    using Eigen = typename Basis<N,D>::Eigen;
+
+
+    template<int N, int D>
+    struct Basis { 
+      struct Canonical {
+        using scalar_type = real_t;
+        using potential_type = GMatrix<rD_to_r<D>, N, N>;
+        using jacobian_type = GMatrix<rD_to_rD<D>, N, N>;
+        using hessian_type = GMatrix<rD_to_rDxD<D>, N, N>;
+        using transformation_type = rD_to_cNxN<D, N>;
+        using local_quadratic_type = rD_to_function_matrix<D, N, rD_to_r<D> >;
+        
+        using potential_evaluation_type = RMatrix<N, N>;
+        using jacobian_evaluation_type = GMatrix<RVector<D>, N, N>;
+        using hessian_evaluation_type = GMatrix<RMatrix<D, D>, N, N>;
+        
+        using potential_return_type = real_t;
+        using jacobian_return_type = RVector<D>;
+        using hessian_return_type = RMatrix<D, D>;
+      };
+
+      struct Eigen {
+        using scalar_type = complex_t;
+        using potential_type = GVector<rD_to_c<D>, N>;
+        using jacobian_type = GVector<rD_to_cD<D>, N>;
+        using hessian_type = GVector<rD_to_cDxD<D>, N>;
+        using transformation_type = rD_to_cNxN<D, N>;
+        using local_quadratic_type = rD_to_function_vector<D, N, rD_to_c<D> >;
+        
+        using potential_evaluation_type = CVector<N>;
+        using jacobian_evaluation_type = GVector<CVector<D>, N>;
+        using hessian_evaluation_type = GVector<CMatrix<D, D>, N>;
+        
+        using potential_return_type = complex_t;
+        using jacobian_return_type = CVector<D>;
+        using hessian_return_type = CMatrix<D, D>;
+      };
+    };
+    template<int D>
+    struct Basis<1,D> {
+      struct Canonical {
+        using scalar_type = real_t;
+        using potential_type = rD_to_r<D>;
+        using jacobian_type = rD_to_rD<D>;
+        using hessian_type = rD_to_rDxD<D>;
+        using local_quadratic_type = rD_to_function<D, rD_to_r<D> >;
+        
+        using potential_evaluation_type = real_t;
+        using jacobian_evaluation_type = RVector<D>;
+        using hessian_evaluation_type = RMatrix<D, D>;
+        
+        using potential_return_type = real_t;
+        using jacobian_return_type = RVector<D>;
+        using hessian_return_type = RMatrix<D, D>;
+      };
+      
+      //~ using Eigen = Canonical;
+      struct Eigen {
+        using scalar_type = complex_t;
+        using potential_type = rD_to_c<D>;
+        using jacobian_type = rD_to_cD<D>;
+        using hessian_type = rD_to_cDxD<D>;
+        using local_quadratic_type = rD_to_function<D, rD_to_c<D> >;
+        
+        using potential_evaluation_type = complex_t;
+        using jacobian_evaluation_type = CVector<D>;
+        using hessian_evaluation_type = CMatrix<D, D>;
+        
+        using potential_return_type = complex_t;
+        using jacobian_return_type = CVector<D>;
+        using hessian_return_type = CMatrix<D, D>;
+       
+      };
+    };
+  }
+}
--- matrixPotentials/test.cpp
+++ matrixPotentials/test.cpp
@@ -1,26 +1,10 @@
 #include <vector>
 #include <iostream>
-#include <array>
-#include <list>
 #include "types.hpp"
-#include "utilities.hpp"
-#include "Bases.hpp"
-#include "Evaluation.hpp"
-#include "Exponential.hpp"
-#include "LocalRemainder.hpp"
-#include "LocalQuadratic.hpp"
-
-template<int N, int D>
-using MatrixPotential = LocalRemainderHomogenousImplementation<EvaluationImplementation<CanonicalBasis,N,D>,EvaluationImplementation<EigenBasis,1,D>,N,D>;
-
-template<int N, int D>
-using InhomogenousMatrixPotential = LocalRemainderInhomogenousImplementation<EvaluationImplementation<CanonicalBasis,N,D>,EvaluationImplementation<EigenBasis,N,D>,N,D>;
-
-template<template<int,int> class Basis, int N, int D>
-using FullTest = ExponentialImplementation<LocalQuadraticImplementation<EvaluationImplementation<Basis,N,D>,Basis,N,D>,Basis,N,D>;
-
-
+#include "bases.hpp"
+#include "potentials.hpp"
    
+using namespace matrixPotentials;  
 int main()
 {
   // Testing
@@ -163,23 +147,28 @@ int main()
     return result;
   };
   
-  rD_to_cNxN<D, N> basis_transform;
+  rD_to_c<D> leading_level = [=] (RVector<D> x) {
+    return x[0]*x[1]+x[2];
+  };
   
-  //~ MatrixPotential<EigenBasis, 2, 3> eigen_test( eigen_potential, eigen_jacobian,
-      //~ eigen_hessian );
-      //~ 
-  //~ MatrixPotential<CanonicalBasis, 2, 3> test(
-    //~ canonical_potential, canonical_jacobian, canonical_hessian );
-    //~ 
-  //~ FullTest ft(canonical_potential, canonical_jacobian, canonical_hessian, basis_transform);  
-  //~ test.evaluate_local_remainder<std::vector>(
-    //~ std::vector<RVector<3>>( 1, RVector<3>( {1, 2, 3} ) ), RVector<3> {0, 1, 2} );
-    //~ 
-  //~ std::vector<RVector<3>> evalPoints( 3 );
-  //~ evalPoints[0] = RVector<3> {1, 1, 3};
-  //~ evalPoints[1] = RVector<3> {0, 2, 0};
-  //~ evalPoints[2] = RVector<3> {2, 1, 3};
-  //~ test.evaluate_at( evalPoints[0] );
-  //~ test.evaluate( evalPoints );
-  //~ eigen_test.evaluate_at( evalPoints[0] );
+  rD_to_cD<D> leading_jac = [=] ( RVector<D> x ) {
+      return CVector<D>( {2 * x[0], 0, 0} );
+  };
+  rD_to_cDxD<D> leading_hess = [=] (RVector<D> x) {
+        CMatrix<D, D> result;
+    result( 0, 0 ) = 2;
+    result( 0, 1 ) = 0;
+    result( 0, 2 ) = 0;
+    result( 1, 0 ) = 0;
+    result( 1, 0 ) = 0;
+    result( 1, 1 ) = 0;
+    result( 1, 2 ) = 0;
+    result( 2, 0 ) = 0;
+    result( 2, 1 ) = 0;
+    result( 2, 2 ) = 0;
+    return result;
+  };
+  
+  MatrixPotential<N,D> s(canonical_potential,canonical_jacobian,canonical_hessian,leading_level,leading_jac,leading_hess);
+  std::cout<<s.evaluate_local_remainder_at(RVector<D>({1,2,3}),RVector<D>({1,2,3}))<<std::endl;
 }
