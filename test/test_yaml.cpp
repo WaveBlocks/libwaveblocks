802fb936a54dd803e007f55f28cea616b7987ce4
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-09-24 19:41:55 +0200

    Enable encoding of complex numbers

--- test/test_yaml.cpp
+++ test/test_yaml.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <yaml-cpp/yaml.h>
 
 #include <waveblocks/basic_types.hpp>
@@ -11,8 +12,29 @@ int main(int argc, char* argv[])
     (void) argc;
     (void) argv;
 
-    YAML::Node config = YAML::LoadFile("config.yaml");
+    // Load from an existing yaml document
+    std::cout << "Loading a yaml file:" << std::endl;
 
-    const complex_t c = config["co"].as<complex_t>();
-    std::cout << "Complex number c: " << c << std::endl;
+    YAML::Node config = YAML::LoadFile("config.yaml");
+    const complex_t c1 = config["c1"].as<complex_t>();
+
+    std::cout << "Complex number c1: " << c1 << std::endl;
+
+
+    // Create a new yaml document
+    std::cout << "Creating a yaml file:" << std::endl;
+
+    const complex_t cn = complex_t(2, 3);
+
+    YAML::Node anode;
+    anode["cN"] = cn;
+
+    YAML::Emitter out;
+    out << config;
+    out << anode;
+
+    std::cout << out.c_str() << std::endl;
+
+    std::ofstream fout("test.yaml");
+    fout << out.c_str() << std::endl;
 }
