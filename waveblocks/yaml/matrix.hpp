a5825e148384ba6ad7678387b2d938d2b56d4822
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-09-25 18:00:18 +0200

    Generalize Eigen serializer even more

--- waveblocks/yaml/matrix.hpp
+++ waveblocks/yaml/matrix.hpp
@@ -5,10 +5,10 @@
 
 
 namespace YAML {
-    template<class S, int R, int C>
-    struct convert<Eigen::Matrix<S,R,C>> {
+    template<class _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+    struct convert<Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>> {
 
-        static Node encode(const Eigen::Matrix<S,R,C> & M) {
+        static Node encode(const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & M) {
             YAML::Node matrixnode;
             for (int r = 0; r < M.rows(); r++) {
                 YAML::Node rownode;
@@ -20,13 +20,13 @@ namespace YAML {
             return matrixnode;
         }
 
-        static bool decode(const Node& matrixnode, Eigen::Matrix<S,R,C> & M) {
+        static bool decode(const Node& matrixnode, Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & M) {
             if (matrixnode.size() != (std::size_t)M.rows()) throw std::runtime_error("Matrix row size mismatch.");
             for (int r = 0; r < M.rows(); r++) {
                 YAML::Node const& rownode = matrixnode[r];
                 if (rownode.size() != (std::size_t)M.cols()) throw std::runtime_error("Matrix col size mismatch.");
                 for (int c = 0; c < M.cols(); c++) {
-                    M(r,c) = rownode[c].as<S>();
+                    M(r,c) = rownode[c].as<_Scalar>();
                 }
             }
             return true;
