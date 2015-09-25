8a7a079f8f9edf70235cbbe64f837581f65448ec
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-09-25 16:43:38 +0200

    Adapt for dynamic matrices

--- waveblocks/yaml/matrix.hpp
+++ waveblocks/yaml/matrix.hpp
@@ -10,9 +10,9 @@ namespace YAML {
 
         static Node encode(const Eigen::Matrix<S,R,C> & M) {
             YAML::Node matrixnode;
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < M.rows(); r++) {
                 YAML::Node rownode;
-                for (int c = 0; c < C; c++) {
+                for (int c = 0; c < M.cols(); c++) {
                     rownode[c] = M(r,c);
                 }
                 matrixnode[r] = rownode;
@@ -21,11 +21,11 @@ namespace YAML {
         }
 
         static bool decode(const Node& matrixnode, Eigen::Matrix<S,R,C> & M) {
-            if (matrixnode.size() != R) throw std::runtime_error("Matrix row size mismatch.");
-            for (int r = 0; r < R; r++) {
+            if (matrixnode.size() != M.rows()) throw std::runtime_error("Matrix row size mismatch.");
+            for (int r = 0; r < M.rows(); r++) {
                 YAML::Node const& rownode = matrixnode[r];
-                if (rownode.size() != C) throw std::runtime_error("Matrix col size mismatch.");
-                for (int c = 0; c < C; c++) {
+                if (rownode.size() != M.cols()) throw std::runtime_error("Matrix col size mismatch.");
+                for (int c = 0; c < M.cols(); c++) {
                     M(r,c) = rownode[c].as<S>();
                 }
             }
