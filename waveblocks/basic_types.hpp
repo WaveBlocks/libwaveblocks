d4d902fea87da24847d2798d0add881f1476b797
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-09-29 18:10:30 +0200

    Store coefficients in complex Eigen matrices

--- waveblocks/basic_types.hpp
+++ waveblocks/basic_types.hpp
@@ -25,8 +25,7 @@ using RArray = Eigen::Array<real_t,R,C>;
 template<int N>
 using HaWpBasisVector = CArray<Eigen::Dynamic, N>;
 
-
-using Coefficients = std::vector<complex_t>;
+using Coefficients = Eigen::Matrix<complex_t, Eigen::Dynamic, 1>;
 }
 
 #endif
