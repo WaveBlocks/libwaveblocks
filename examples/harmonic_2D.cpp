849dc83a1cbfcaf1a03b812c8de8531b66b421e3
Lionel Miserez <lionel.miserez@gess.ethz.ch>
2015-09-30 11:35:30 +0200

    Included packet writing in harmonic_2D.cpp

--- examples/harmonic_2D.cpp
+++ examples/harmonic_2D.cpp
@@ -10,13 +10,17 @@
 #include "waveblocks/shape_enumerator.hpp"
 #include "waveblocks/shape_hypercubic.hpp"
 #include "waveblocks/hawp_paramset.hpp"
-
+#include "waveblocks/utilities/packetWriter.hpp"
+#include "waveblocks/hawp_gradient_operator.hpp"
+#include "waveblocks/hawp_gradient_evaluator.hpp"
+#include "waveblocks/utilities/energy.hpp"
 
 using namespace waveblocks;
+
 int main() {
   const int N = 1;
   const int D = 2;
-  const int K = 3;
+  const int K = 5;
   const real_t sigma_x = 0.5;
   const real_t sigma_y = 0.5;
 
@@ -25,8 +29,8 @@ int main() {
 
   const real_t eps = 0.1;
 
-  using MultiIndex = TinyMultiIndex<unsigned short, D>;
-  
+  using MultiIndex = TinyMultiIndex<unsigned long, D>;
+
   // The parameter set of the initial wavepacket
   CMatrix<D,D> Q = CMatrix<D,D>::Identity();
   CMatrix<D,D> P = complex_t(0,1)*CMatrix<D,D>::Identity();
@@ -41,7 +45,6 @@ int main() {
   HaWpParamSet<D> param_set(q,p,Q,P);
   Coefficients coeffs = Coefficients::Ones(std::pow(K, D), 1);
   ScalarHaWp<D,MultiIndex> packet;
-
 
   packet.eps() = eps;
   packet.parameters() = param_set;
@@ -58,7 +61,7 @@ int main() {
       return CVector<D>{sigma_x*x[0], sigma_y*x[1]};;
     };
   typename ScalarLeadingLevel<D>::hessian_type leading_hess =
-    [sigma_x,sigma_y](CVector<D> x) {
+    [sigma_x,sigma_y](CVector<D>) {
       CMatrix<D,D> res;
       res(0,0) = sigma_x;
       res(1,1) = sigma_y;
@@ -76,33 +79,16 @@ int main() {
 
 
   // Preparing the file
-  std::ofstream csv;
-  csv.open ("harmonic_2D.out");
-  csv << "t, p, q, P, Q, S";
+ utilities::PacketWriter<ScalarHaWp<D,MultiIndex>> writer("harmonic_2D.out");
 
-  
-  csv << 0 << "," << param_set.p << "," << param_set.q << "," << param_set.P << "," << param_set.Q << "," << S << std::endl;
-  
   // Propagation
   for (real_t t = 0; t < T; t += dt) {
     propagator.propagate(packet,dt,V,S);
-    const auto& params = packet.parameters();
-    csv << t << "," << params.p << "," <<
-     params.q << "," << params.P << "," <<
-     params.Q << "," << S  << std::endl;
+    writer.store_packet(t,packet,S);
+    real_t kinetic = kinetic_energy<D,MultiIndex>(packet);
+    real_t potential = potential_energy<ScalarMatrixPotential<D>,D,MultiIndex, TQR>(packet,V);
+    real_t total = kinetic+potential;
+    std::cout << t << "," << potential << "," << kinetic << ", "<< total << std::endl;
+    std::cout << packet.parameters() << std::endl;
   }
-  
-  
-
-  csv.close();
-
-
-  
-  
-
-  
-
-
-
-
 }
