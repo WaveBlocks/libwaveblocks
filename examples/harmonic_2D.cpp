faa942100238022f7e0f1e1cf16b446d9e77d195
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-09-30 15:49:30 +0200

    Propagate a Gaussian packet

--- examples/harmonic_2D.cpp
+++ examples/harmonic_2D.cpp
@@ -19,9 +19,10 @@ using namespace waveblocks;
 int main() {
   const int N = 1;
   const int D = 2;
-  const int K = 20;
+  const int K = 5;
   const real_t sigma_x = 0.5;
   const real_t sigma_y = 0.5;
+
   const real_t tol = 1e-10;
 
   const real_t T = 12;
@@ -31,70 +32,77 @@ int main() {
 
   using MultiIndex = TinyMultiIndex<unsigned long, D>;
 
-  // The parameter set of the initial wavepacket
+  // Parameter set PI
   CMatrix<D,D> Q = CMatrix<D,D>::Identity();
-  CMatrix<D,D> P = complex_t(0,1)*CMatrix<D,D>::Identity();
-  RVector<D> q = {-3.0,0.0};
-  RVector<D> p = {0.0,0.5};
+  CMatrix<D,D> P = complex_t(0,1) * CMatrix<D,D>::Identity();
+  RVector<D> q = {-3.0, 0.0};
+  RVector<D> p = { 0.0, 0.5};
   complex_t S = 0.;
-
-  // Setting up the wavepacket
-  ShapeEnumerator<D, MultiIndex> enumerator;
-  ShapeEnum<D, MultiIndex> shape_enum =
-    enumerator.generate(HyperCubicShape<D>(K));
   HaWpParamSet<D> param_set(q,p,Q,P);
-  Coefficients coeffs = Coefficients::Ones(std::pow(K, D), 1);
-  ScalarHaWp<D,MultiIndex> packet;
 
+  // Basis shape
+  ShapeEnumerator<D, MultiIndex> enumerator;
+  ShapeEnum<D, MultiIndex> shape_enum = enumerator.generate(HyperCubicShape<D>(K));
+
+  // Gaussian Wavepacket phi_00 with c_00 = 1
+  Coefficients coeffs = Coefficients::Zero(std::pow(K, D), 1);
+  coeffs(0,0) = 1.0;
+
+  // Assemble packet
+  ScalarHaWp<D,MultiIndex> packet;
   packet.eps() = eps;
   packet.parameters() = param_set;
   packet.shape() = std::make_shared<ShapeEnum<D,MultiIndex>>(shape_enum);
   packet.coefficients() = coeffs;
 
   // Defining the potential
-  typename CanonicalBasis<N,D>::potential_type potential = [sigma_x,sigma_y](CVector<D> x) {
-    return 0.5*(sigma_x*x[0]*x[0] + sigma_y*x[1]*x[1]).real();
+  typename CanonicalBasis<N,D>::potential_type potential =
+      [sigma_x,sigma_y](CVector<D> x) {
+      return 0.5*(sigma_x*x[0]*x[0] + sigma_y*x[1]*x[1]).real();
   };
   typename ScalarLeadingLevel<D>::potential_type leading_level = potential;
   typename ScalarLeadingLevel<D>::jacobian_type leading_jac =
-    [sigma_x,sigma_y](CVector<D> x) {
-      return CVector<D>{sigma_x*x[0], sigma_y*x[1]};;
-    };
+      [sigma_x,sigma_y](CVector<D> x) {
+      return CVector<D>{sigma_x*x[0], sigma_y*x[1]};
+  };
   typename ScalarLeadingLevel<D>::hessian_type leading_hess =
-    [sigma_x,sigma_y](CVector<D>) {
+      [sigma_x,sigma_y](CVector<D>) {
       CMatrix<D,D> res;
       res(0,0) = sigma_x;
       res(1,1) = sigma_y;
       return res;
-    };
-    
-    
+  };
+
   ScalarMatrixPotential<D> V(potential,leading_level,leading_jac,leading_hess);
 
   // Quadrature rules
-  using TQR = waveblocks::TensorProductQR < waveblocks::GaussHermiteQR<3>,
-              waveblocks::GaussHermiteQR<4>>;
+  using TQR = waveblocks::TensorProductQR<
+      waveblocks::GaussHermiteQR<3>,
+      waveblocks::GaussHermiteQR<4>>;
+
   // Defining the propagator
   propagators::Hagedorn<N,D,MultiIndex, TQR> propagator;
 
-
   // Preparing the file
- utilities::PacketWriter<ScalarHaWp<D,MultiIndex>> writer("harmonic_2D.out");
+  utilities::PacketWriter<ScalarHaWp<D,MultiIndex>> writer("harmonic_2D.out");
 
   // Propagation
   for (real_t t = 0; t < T; t += dt) {
     propagator.propagate(packet,dt,V,S);
+    std::cout << t << std::endl;
     writer.store_packet(t,packet,S);
     real_t kinetic = kinetic_energy<D,MultiIndex>(packet);
     real_t potential = potential_energy<ScalarMatrixPotential<D>,D,MultiIndex, TQR>(packet,V);
     real_t total = kinetic+potential;
     std::cout << t << ", " << potential << ", " << kinetic << ", "<< total << std::endl;
+
     bool flag = true;
     for (int i = 0; i < std::pow(K,D); ++i) {
       auto diff = packet.coefficients()[i] - complex_t(1,0);
       if (diff.real() > tol || diff.imag() > tol)
         flag = false;
       }
+
     std::cout << "coefficients constant? " << (flag ? "yes" : "no") << std::endl;
     std::cout << packet.parameters() << std::endl;
   }
