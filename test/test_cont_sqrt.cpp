bc9d6d7e84359b5d351edaf10e6aec18d9319a13
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-10-05 15:19:29 +0200

    New test for cont_sqrt

--- test/test_cont_sqrt.cpp
+++ test/test_cont_sqrt.cpp
@@ -12,54 +12,24 @@ using namespace waveblocks;
 int main()
 {
     real_t PI = 3.14159265359;
-    
+    int N = 1000;
+
     ContinuousSqrt<real_t> sqrt;
-    
-    //counter-clock-wise
-    {
-        std::ofstream out("cont_sqrt_ccw.csv");
-        out << 
-                std::setw(15) << "arg(in)" << " " << 
-                std::setw(15) << "real(out)" << " " << 
-                std::setw(15) << "imag(out)" << " " <<
-                std::setw(15) << "abs(out)" << " " <<
-                std::setw(15) << "arg(out)" << "\n";
-        for (int i = 0; i <= 4*360; i += 10) {
-            real_t angle = i*PI/real_t(180);
-            
-            complex_t result = sqrt( std::polar(1.0, angle) );
-            
-            out << 
-                std::setw(15) << angle << " " << 
-                std::setw(15) << std::real(result) << " " << 
-                std::setw(15) << std::imag(result) << " " <<
-                std::setw(15) << std::abs(result) << " " <<
-                std::setw(15) << std::arg(result) << "\n";
-        }
+
+    std::ofstream out("cont_sqrt.csv");
+    out.precision(15);
+
+    for (int i = 0; i < N; i++) {
+        real_t angle = 4*PI * i / (N-1);
+        complex_t y = std::polar(2.0+0.1*angle, angle);
+        complex_t sy = sqrt(y);
+
+        out << std::setw(20) << angle << ", " <<
+            std::setw(20) << std::real(y) << ", " <<
+            std::setw(20) << std::imag(y) << ", " <<
+            std::setw(20) << std::real(sy) << ", " <<
+            std::setw(20) << std::imag(sy) << std::endl;
     }
-    
-    //clock-wise
-    {
-        std::ofstream out("cont_sqrt_cw.csv");
-        out << 
-                std::setw(15) << "arg(in)" << " " << 
-                std::setw(15) << "real(out)" << " " << 
-                std::setw(15) << "imag(out)" << " " <<
-                std::setw(15) << "abs(out)" << " " <<
-                std::setw(15) << "arg(out)" << "\n";
-        for (int i = 4*360; i >= 0; i -= 10) {
-            real_t angle = i*PI/real_t(180);
-            
-            complex_t result = sqrt( std::polar(1.0, angle) );
-            
-            out << 
-                std::setw(15) << angle << " " << 
-                std::setw(15) << std::real(result) << " " << 
-                std::setw(15) << std::imag(result) << " " <<
-                std::setw(15) << std::abs(result) << " " <<
-                std::setw(15) << std::arg(result) << "\n";
-        }
-    }
-    
+
     return 0;
 }
