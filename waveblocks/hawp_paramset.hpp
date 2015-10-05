0a5043e1c638c3e8e23acf713f3eb44e72c65a33
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-10-05 16:12:08 +0200

    Correct resync of sqrt(det(Q))

--- waveblocks/hawp_paramset.hpp
+++ waveblocks/hawp_paramset.hpp
@@ -135,7 +135,7 @@ public:
      * Compute the continuous square root of \f$ \det Q \f$ after an update
      * of the \f$ Q \f$ parameter.
      */
-    inline void resync() {sqrt_detQ_ = ContinuousSqrt<real_t>(std::sqrt(Q_.determinant()));}
+    inline void resync() {sqrt_detQ_(Q_.determinant());}
 
     /**
      * Check the compatibility relations
