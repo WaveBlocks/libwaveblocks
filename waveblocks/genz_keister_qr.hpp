773f565ab92a2693a608d85d91716cca8534575f
Benedek Vartok <vartokb@student.ethz.ch>
2015-11-18 20:27:23 +0100

    implemented Genz-Keister nodes
    - weights yet to do

--- waveblocks/genz_keister_qr.hpp
+++ waveblocks/genz_keister_qr.hpp
@@ -8,7 +8,7 @@
 #include <Eigen/Dense>
 
 #include "basic_types.hpp"
-#include "tables_gausshermite.hpp"
+#include "tables_genzkeister.hpp"
 #include "util/combinatorics.hpp"
 
 namespace waveblocks {
@@ -16,6 +16,10 @@ namespace waveblocks {
 
 /**
  * \brief Structure providing weighted nodes for Genz-Keister quadrature.
+ *
+ * \tparam D dimensionality of the Genz-Keister rule
+ * \tparam LEVEL the level of the Genz-Keister rule, must be between 1 and 30
+ *   inclusive
  */
 template <dim_t D, dim_t LEVEL>
 struct GenzKeisterQR
@@ -85,15 +89,96 @@ private:
     {
         clear_cache();
 
-        const dim_t K = LEVEL - 1;
-        const auto parts = partitions<D>(K);
-        std::cout << "Partitions:\n";
-        for (const auto& part : parts)
+        // Check LEVEL validity.
+        if (LEVEL < 1 || LEVEL > 30)
         {
-            std::cout << part << "\n";
+            std::cerr << "GenzKeisterQR: LEVEL must be between 1 and 30!\n";
         }
 
+        const dim_t K = LEVEL - 1;
+        const partitions_t<D> parts = partitions<D>(K);
+        dim_t node_offset = 0;
+        for (const auto& part : parts)
+        {
+            int sum = 0;
+            for (int p : part) sum += p + genz_keister_zs(p);
+            if (sum <= K)
+            {
+                // Calculate nodes and weights for this partition.
+                const NodeMatrix part_nodes = nodes_for_partition(part);
+                const dim_t n_nodes = part_nodes.cols();
+                const real_t part_weight = weight_for_partition(part);
+
+                // DEBUG
+                std::cout << "\nPart:";
+                for (int p : part) std::cout << " " << p;
+                std::cout << "\nNodes:\n" << part_nodes << "\n";
+                std::cout << "\nWeight:\n" << part_weight << "\n";
+
+                // Append them to the result.
+                cached_nodes.conservativeResize(Eigen::NoChange, node_offset + n_nodes);
+                cached_weights.conservativeResize(Eigen::NoChange, node_offset + n_nodes);
+
+                cached_nodes.rightCols(n_nodes) = part_nodes;
+                cached_weights.tail(n_nodes).fill(part_weight);
+
+                node_offset += n_nodes;
+            }
+        }
+
+        // Transform weights.
+        //cached_weights /= exp(-
+
         cached = true;
+    }
+
+    /**
+     * \brief Return fully symmetric quadrature nodes for given partition.
+     *
+     * \param[in] part partition
+     */
+    static NodeMatrix nodes_for_partition(const partition_t<D>& part)
+    {
+        const dim_t xi = nz<D>(part);
+        const permutations_t<D> perms = permutations<D>(part);
+        NodeMatrix nodes = NodeMatrix::Zero(D, perms.size() * (1 << (D-xi)));
+
+        dim_t node_offset = 0;
+        for (const auto& perm : perms)
+        {
+            // Copy nodes with flipped signs from non-zero generators.
+            int nnz_row = 0;
+            for (int row = 0; row < D; ++row)
+            {
+                if (perm[row] == 0) continue;
+
+                for (int col = 0; col < (1 << (D-xi)); ++col)
+                {
+                    // Generators corresponding to current permutation.
+                    const int sign = -2 * ((col >> nnz_row) & 1) + 1;
+                    nodes(row, node_offset + col) = sign *
+                        genz_keister_generators(perm[row]);
+                }
+
+                // Count non-zero rows.
+                ++ nnz_row;
+            }
+
+            node_offset += (1 << (D-xi));
+        }
+
+        return nodes;
+    }
+
+    /**
+     * \brief Return quadrature weight for given partition.
+     *
+     * \param[in] part partition
+     */
+    static real_t weight_for_partition(const partition_t<D>& part)
+    {
+        // TODO
+        return 0;
     }
 };
 
--- waveblocks/util/combinatorics.hpp
+++ waveblocks/util/combinatorics.hpp
@@ -14,6 +14,10 @@
 template<int D> using partition_t = std::array<int, D>;
 template<int D> using partitions_t = std::list<partition_t<D>>;
 template<int D> using point_t = std::array<int, D>;
+template<int D> using latticepoints_t = std::list<point_t<D>>;
+template<int D> using permutation_t = std::array<int, D>;
+template<int D> using permutations_t = std::list<permutation_t<D>>;
+
 
 template<int D>
 int sum(const point_t<D> Z) {
@@ -25,6 +29,29 @@ int sum(const point_t<D> Z) {
     }
     return s;
 }
+
+
+template<int D>
+int nz(const point_t<D> Z) {
+    /* Number of zero components of the point Z.
+     */
+    int s = 0;
+    for(int i=0; i<D; i++) {
+        if(Z[i] == 0) {
+            s++;
+        }
+    }
+    return s;
+}
+
+
+template<int D>
+int nnz(const point_t<D> Z) {
+    /* Number of non-zero components of the point Z.
+     */
+    return D - nz<D>(Z);
+}
+
 
 template<int D>
 partitions_t<D> partitions(const int K) {
@@ -67,4 +94,98 @@ partitions_t<D> partitions(const int K) {
     }
 
     return partitions;
+}
+
+
+template<int D>
+latticepoints_t<D> lattice_points(const int N) {
+    /* This method enumerates all lattice points of a lattice
+     * :math:`\Lambda \subset \mathbb{N}^D` in :math:`D` dimensions
+     * having fixed :math:`l_1` norm :math:`N`.
+     *
+     * N: The :math:`l_1` norm of the lattice points.
+     */
+    latticepoints_t<D> L;
+    point_t<D> k;
+
+    for(int n=0; n <= N; n++) {
+        k.fill(0);
+        k[0] = n;
+        L.push_back(k);
+        int c = 1;
+        while(k[D-1] < n) {
+            if(c == D) {
+                for(int i = c-1; i > 0; i--) {
+                    c = i;
+                    if(k[i-1] != 0) {
+                        break;
+                    }
+                }
+            }
+            k[c-1]--;
+            c++;
+            k[c-1] = n;
+            for(int t=0; t < c-1; t++) {
+                k[c-1] -= k[t];
+            }
+            if(c < D) {
+                for(int t=c; t < D; t++) {
+                    k[t] = 0;
+                }
+            }
+            L.push_back(k);
+        }
+    }
+
+    return L;
+}
+
+
+template<int D>
+permutations_t<D> permutations(const permutation_t<D> permutation) {
+    /* Enumerate all permutations in anti-lexicographical
+     * order follwing the given permutation `P`.
+     *
+     * P: A permutation
+     */
+    permutations_t<D> permutations;
+    permutation_t<D> P = permutation;
+
+    permutations.push_back(P);
+
+    bool broke = true;
+    while(broke) {
+        broke = false;
+        for(int i=1; i < D; i++) {
+            int pi = P[i];
+            if(P[i-1] > pi) {
+                int I = i;
+                if(i > 1) {
+                    int J = I;
+                    int u = I / 2;
+                    for(int j=0; j < u; j++) {
+                        int pj = P[j];
+                        if(pj <= pi) {
+                            I--;
+                        }
+                        P[j] = P[i-j-1];
+                        P[i-j-1] = pj;
+                        if(P[j] > pi) {
+                            J = j + 1;
+                        }
+                    }
+                    if(P[I-1] <= pi) {
+                        I = J;
+                    }
+                }
+                P[i] = P[I-1];
+                P[I-1] = pi;
+                permutations.push_back(P);
+                broke = true;
+                break;
+            }
+        }
+    }
+
+    return permutations;
 }
