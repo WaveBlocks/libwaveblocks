ddd64935ddd1bd288a721cb0e684e3255c1cda40
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-12-09 15:44:17 +0100

    Document observables namespace

---
+++ waveblocks/observables/observables.hpp
@@ -0,0 +1,9 @@
+#error Documentation only
+
+namespace waveblocks {
+    /**
+     * @namespace observables
+     * Namespace for functionality to compute observables.
+     */
+    namespace observables {}
+}
