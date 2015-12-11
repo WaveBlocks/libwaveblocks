9a90f6398e91facf1fe7b53988a02014d7d94c3a
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-12-11 15:43:10 +0100

    Remove duplicate namespace

--- waveblocks/potentials/potentials.hpp
+++ waveblocks/potentials/potentials.hpp
@@ -8,35 +8,28 @@
 
 namespace waveblocks
 {
-  namespace potentials
-  {
     namespace potentials
     {
+        template<int N, int D>
+        using InhomogenousMatrixPotential = modules::Inhomogenous<N,D>;
 
-      template<int N, int D>
-      using InhomogenousMatrixPotential =
-        modules::Inhomogenous<N,D>;
+        template<int N, int D>
+        using HomogenousMatrixPotential = modules::Homogenous<N,D>;
 
-      template<int N, int D>
-      using HomogenousMatrixPotential =
-        modules::Homogenous<N,D>;
-
-      template<int D>
-      using ScalarMatrixPotential = HomogenousMatrixPotential<1,D>;
+        template<int D>
+        using ScalarMatrixPotential = HomogenousMatrixPotential<1,D>;
     }
-  }
 
-  using potentials::potentials::InhomogenousMatrixPotential;
-  using potentials::potentials::HomogenousMatrixPotential;
-  using potentials::potentials::ScalarMatrixPotential;
+    using potentials::InhomogenousMatrixPotential;
+    using potentials::HomogenousMatrixPotential;
+    using potentials::ScalarMatrixPotential;
 
+    template<int N, int D>
+    using InhomogenousLeadingLevel = potentials::modules::LocalQuadratic<potentials::bases::Eigen<N,D>>;
 
-  template<int N, int D>
-  using InhomogenousLeadingLevel = potentials::modules::LocalQuadratic<potentials::bases::Eigen<N,D>>;
+    template<int N, int D>
+    using HomogenousLeadingLevel = potentials::modules::LocalQuadratic<potentials::bases::Eigen<1,D>>;
 
-  template<int N, int D>
-  using HomogenousLeadingLevel = potentials::modules::LocalQuadratic<potentials::bases::Eigen<1,D>>;
-
-  template<int D>
-  using ScalarLeadingLevel = HomogenousLeadingLevel<1,D>;
+    template<int D>
+    using ScalarLeadingLevel = HomogenousLeadingLevel<1,D>;
 }
