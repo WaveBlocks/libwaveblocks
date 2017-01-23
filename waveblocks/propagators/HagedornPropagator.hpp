5e68f98f7706dd1a40c637791c771b6ae069da23
Matthias Untergassmair <untergam@student.ethz.ch>
2017-01-23 00:15:49 +0000

    Magnus Propagator

--- waveblocks/propagators/HagedornPropagator.hpp
+++ waveblocks/propagators/HagedornPropagator.hpp
@@ -13,13 +13,13 @@ namespace utils = utilities;
  *
  * This class implements the (pre-/post-)propagate functions for the Hagedorn Propagator
  */
-template <int N, int D, typename MultiIndex_t, typename MDQR_t, typename Potential_t, typename Packet_t>
-class HagedornPropagator : public Propagator<HagedornPropagator<N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t>,N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t> {
+template <int N, int D, typename MultiIndex_t, typename MDQR_t, typename Potential_t, typename Packet_t, typename Coef_t = SplitCoefs<0,0>>
+class HagedornPropagator : public Propagator<HagedornPropagator<N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t,Coef_t>,N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t,Coef_t> {
 
 	public:
 
 		// inherit constructor
-		using Propagator<HagedornPropagator<N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t>,N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t>::Propagator;
+		using Propagator<HagedornPropagator<N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t,Coef_t>,N,D,MultiIndex_t,MDQR_t,Potential_t,Packet_t,Coef_t>::Propagator;
 
 		std::string getName() { return "Hagedorn"; }
 
