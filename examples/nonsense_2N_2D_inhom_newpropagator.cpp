a11dad449238f1d92bd5b4315b1a54e0ba5220c4
Matthias Untergassmair <untergam@student.ethz.ch>
2017-02-03 18:55:43 +0100

    Example adapted: nonsense simulations

--- examples/nonsense_2N_2D_inhom_newpropagator.cpp
+++ examples/nonsense_2N_2D_inhom_newpropagator.cpp
@@ -21,9 +21,13 @@
 
 using namespace waveblocks;
 
+namespace split = propagators::splitting_parameters;
+
 int main() {
 
-	// TODO: implement with new propagators
+	////////////////////////////////////////////////////
+    // General parameters
+	////////////////////////////////////////////////////
 
     const int N = 2;
     const int D = 2;
@@ -32,37 +36,56 @@ int main() {
     const real_t sigma_y = 0.5;
 
     const real_t T = 12;
-    const real_t dt = 0.01;
+    const real_t Dt = 0.01;
 
     const real_t eps = 0.1;
 
-    using MultiIndex = wavepackets::shapes::TinyMultiIndex<unsigned short, D>;
 
-    // The parameter set of the initial wavepacket
+	////////////////////////////////////////////////////
+    // Composed Types
+	////////////////////////////////////////////////////
+
+    using MultiIndex = wavepackets::shapes::TinyMultiIndex<unsigned short, D>;
+    using TQR = innerproducts::TensorProductQR<innerproducts::GaussHermiteQR<3>,
+                                               innerproducts::GaussHermiteQR<4>>;
+    using Packet_t = wavepackets::InhomogeneousHaWp<D,MultiIndex>;
+    using Potential_t = InhomogenousMatrixPotential<N,D>;
+
+
+	/////////////////////////////////////////////////////
+	// Building the WavePacket
+	/////////////////////////////////////////////////////
+
+    // basis shapes
+    wavepackets::shapes::ShapeEnumerator<D, MultiIndex> enumerator;
+    wavepackets::shapes::ShapeEnum<D, MultiIndex> shape_enum = enumerator.generate(wavepackets::shapes::HyperCubicShape<D>(K));
+
+    // initial parameters
     CMatrix<D,D> Q = CMatrix<D,D>::Identity();
     CMatrix<D,D> P = complex_t(0,1)*CMatrix<D,D>::Identity();
     RVector<D> q = {-3.0, 0.0};
     RVector<D> p = {0.0, 0.5};
-
-    // Setting up the wavepacket
-    wavepackets::shapes::ShapeEnumerator<D, MultiIndex> enumerator;
-    wavepackets::shapes::ShapeEnum<D, MultiIndex> shape_enum = enumerator.generate(wavepackets::shapes::HyperCubicShape<D>(K));
     wavepackets::HaWpParamSet<D> param_set(q, p, Q, P, 0.0);
     wavepackets::HaWpParamSet<D> param_set2(2*q, 0.5*p, Q, P, 0.0);
+
+    // initial coefficients
     Coefficients coeffs = Coefficients::Ones(std::pow(K, D), 1);
-    wavepackets::InhomogeneousHaWp<D,MultiIndex> packet(N);
+    Packet_t packet(N);
 
+    // assemble packet
     packet.eps() = eps;
-    packet.component(0).parameters() = param_set;
-
     packet.component(0).shape() = std::make_shared<wavepackets::shapes::ShapeEnum<D,MultiIndex>>(shape_enum);
-    packet.component(0).coefficients() = coeffs;
-
-    packet.component(1).parameters() = param_set2;
     packet.component(1).shape() = std::make_shared<wavepackets::shapes::ShapeEnum<D,MultiIndex>>(shape_enum);
+    packet.component(0).parameters() = param_set;
+    packet.component(1).parameters() = param_set2;
+    packet.component(0).coefficients() = coeffs;
     packet.component(1).coefficients() = coeffs;
 
+
+	/////////////////////////////////////////////////////
     // Defining the potential
+	/////////////////////////////////////////////////////
+
     typename CanonicalBasis<N,D>::potential_type potential;
 
     potential(0,0) = [sigma_x,sigma_y,N](CVector<D> x) {
@@ -103,16 +126,30 @@ int main() {
     leading_jac(1) = leading_jac(0);
     leading_hess(1) = leading_hess(0);
 
-    InhomogenousMatrixPotential<N,D> V(potential,leading_level,leading_jac,leading_hess);
+    Potential_t V(potential,leading_level,leading_jac,leading_hess);
 
-    // Quadrature rules
-    using TQR = innerproducts::TensorProductQR<innerproducts::GaussHermiteQR<3>,
-                                               innerproducts::GaussHermiteQR<4>>;
-    // Defining the propagator
-    propagators::Hagedorn<N,D,MultiIndex, TQR> propagator;
 
-    // Propagation
-    for (real_t t = 0; t < T; t += dt) {
-        propagator.propagate(packet,dt,V);
-    }
+	////////////////////////////////////////////////////
+	// Defining the Propagator
+	////////////////////////////////////////////////////
+
+	propagators::HagedornPropagator<N,D,MultiIndex,TQR,Potential_t,Packet_t> pHagedorn(packet,V);
+	propagators::SemiclassicalPropagator<N,D,MultiIndex,TQR,Potential_t,Packet_t,propagators::SplitCoefs<1,1>> pSemiclassical(packet,V,split::coefLT);
+	propagators::MG4Propagator<N,D,MultiIndex,TQR,Potential_t,Packet_t,propagators::SplitCoefs<1,1>> pMG4(packet,V,split::coefLT);
+	propagators::Pre764Propagator<N,D,MultiIndex,TQR,Potential_t,Packet_t,propagators::SplitCoefs<1,1>> pPre764(packet,V,split::coefLT);
+	propagators::McL42Propagator<N,D,MultiIndex,TQR,Potential_t,Packet_t,propagators::SplitCoefs<1,1>> pMcL42(packet,V,split::coefLT);
+	propagators::McL84Propagator<N,D,MultiIndex,TQR,Potential_t,Packet_t,propagators::SplitCoefs<1,1>> pMcL84(packet,V,split::coefLT);
+
+	////////////////////////////////////////////////////
+	// Propagate
+	////////////////////////////////////////////////////
+
+	pHagedorn.evolve(T,Dt);
+	pSemiclassical.evolve(T,Dt);
+	pMG4.evolve(T,Dt);
+	pPre764.evolve(T,Dt);
+	pMcL42.evolve(T,Dt);
+	pMcL84.evolve(T,Dt);
+
+    return 0;
 }
