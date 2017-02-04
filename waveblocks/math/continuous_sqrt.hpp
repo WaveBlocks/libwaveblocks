4734509f2ce8cb9c26b86d224da55090ff1b55f2
Matthias Untergassmair <untergam@student.ethz.ch>
2017-02-04 02:05:07 +0100

    re-introduced continuous_sqrt after bugfix

--- waveblocks/math/continuous_sqrt.hpp
+++ waveblocks/math/continuous_sqrt.hpp
@@ -114,8 +114,6 @@ namespace waveblocks {
              */
             std::complex<T> operator()(std::complex<T> input)
             {
-            	/*
-            	// TODO: uncomment this again to re-introduce comples square root
                 if (empty_) {
                     state_ = 0.5*std::arg(input); // choose principal solution
                     empty_ = false;
@@ -124,8 +122,6 @@ namespace waveblocks {
                 }
 
                 sqrt_ = std::polar(std::sqrt(std::abs(input)), state_);
-                */
-                sqrt_ = std::complex<T>(std::sqrt(std::abs(input)),0);
 
                 return sqrt_;
             }
