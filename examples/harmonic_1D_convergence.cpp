0a5212effcb3292cf7970b7c03ef1845816c7a4e
Matthias Untergassmair <untergam@student.ethz.ch>
2017-02-18 01:47:22 +0100

    Convergence Examples

--- examples/harmonic_1D_convergence.cpp
+++ examples/harmonic_1D_convergence.cpp
@@ -18,8 +18,6 @@
 #include "waveblocks/propagators/Pre764Propagator.hpp"
 #include "waveblocks/propagators/McL42Propagator.hpp"
 #include "waveblocks/propagators/McL84Propagator.hpp"
-#include "waveblocks/observables/energy.hpp"
-#include "waveblocks/io/hdf5writer.hpp"
 
 
 using namespace waveblocks;
@@ -82,7 +80,6 @@ class Parameters_Harmonic_1D {
 		// general parameters
 		const real_t sigma_x;
 		const real_t T;
-		const real_t Dt;
 		const real_t eps;
 
 		// wave packet
@@ -106,12 +103,9 @@ class Parameters_Harmonic_1D {
 		wavepackets::shapes::ShapeEnumerator<D, MultiIndex> enumerator;
 		wavepackets::shapes::ShapeEnum<D, MultiIndex> shape_enum;
 
-		io::hdf5writer<D> writer;
-
-		Parameters_Harmonic_1D(std::string name)
+		Parameters_Harmonic_1D()
 		 : sigma_x(1.)
 		 , T(10)
-		 , Dt(0.05)
 		 , eps(0.01)
 		 , Q(CMatrix<D,D>::Identity())
 		 , P(complex_t(0,1) * CMatrix<D,D>::Identity())
@@ -122,74 +116,105 @@ class Parameters_Harmonic_1D {
 		 , coeffs(Coefficients::Zero(std::pow(K,D),1))
 		 , splitCoefs(propagators::splitting_parameters::coefY4)
 		 , shape_enum(enumerator.generate(wavepackets::shapes::HyperCubicShape<D>(K)))
-		 , writer("harmonic_1D_" + name + ".hdf5")
 		{
 			coeffs[0] = 1.0;
 			packet.eps() = eps;
 			packet.shape() = std::make_shared<wavepackets::shapes::ShapeEnum<D,MultiIndex>>(shape_enum);
 			packet.parameters() = param_set;
 			packet.coefficients() = coeffs;
-
-			writer.set_write_norm(true);
-			writer.set_write_energies(true);
-			writer.prestructuring<MultiIndex>(packet,Dt);
 		}
-
-		~Parameters_Harmonic_1D() {
-			writer.poststructuring();
-		}
-
-		void callback(unsigned /*m*/, real_t /*t*/) {
-			real_t ekin = observables::kinetic_energy<D,MultiIndex>(packet);
-			real_t epot = observables::potential_energy<Potential,D,MultiIndex,QR>(packet,V); // TODO: causes segfault - investigate
-			writer.store_packet(packet);
-			writer.store_norm(packet);
-			writer.store_energies(epot,ekin);
-		}
-
 
 };
 
 int main() {
 
-	using P = Parameters_Harmonic_1D; // all the parameters for the simulation are contained in this class
+	using P = Parameters_Harmonic_1D;
 
-	{ // Hagedorn
-		Parameters_Harmonic_1D param_Hagedorn("Hagedorn");
-		propagators::HagedornPropagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t> pHagedorn(param_Hagedorn.packet,param_Hagedorn.V);
-		pHagedorn.evolve(param_Hagedorn.T,param_Hagedorn.Dt,std::bind(&P::callback,std::ref(param_Hagedorn),_1,_2));
+	const real_t Dt_gold = 2e-4;
+
+	// define a grid for evaluation
+    const int G = 1000;
+    RMatrix<P::D,G> grid(P::D,G);
+    for (int d = 0; d < P::D; d++) {
+        for (int i = 0; i < G; i++) {
+            grid(d,i) = 5*(-1.0 + 2.0*(i+1)/real_t(G));
+        }
+    }
+
+	// define the high-precision propagator and evolve
+	Parameters_Harmonic_1D param_gold;
+	propagators::McL84Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,propagators::SplitCoefs<34,34>> pGold(param_gold.packet,param_gold.V,propagators::splitting_parameters::coefKL10);
+	pGold.evolve(param_gold.T,Dt_gold);
+
+	// evaluate gold solution on grid
+	const CArray<1,G> eval_gold = param_gold.packet.evaluate(grid);
+	real_t norm_gold = 0;
+	for(unsigned i=0; i<G; i++) {
+		real_t x = std::abs(eval_gold[i]);
+		norm_gold += x*x;
 	}
+	norm_gold = std::sqrt(norm_gold);
 
-	// { // Semiclassical
-	// 	Parameters_Harmonic_1D param_Semiclassical("Semiclassical");
-	// 	propagators::SemiclassicalPropagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> pSemiclassical(param_Semiclassical.packet,param_Semiclassical.V,param_Semiclassical.splitCoefs);
-	// 	pSemiclassical.evolve(param_Semiclassical.T,param_Semiclassical.Dt,std::bind(&P::callback,std::ref(param_Semiclassical),_1,_2));
-	// }
-	
-	// { // MG4
-	// 	Parameters_Harmonic_1D param_MG4("MG4");
-	// 	propagators::MG4Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> pMG4(param_MG4.packet,param_MG4.V,param_MG4.splitCoefs);
-	// 	pMG4.evolve(param_MG4.T,param_MG4.Dt,std::bind(&P::callback,std::ref(param_MG4),_1,_2));
-	// }
+	auto errorL2 = [&] (P::Packet_t& pack) {
+		const CArray<1,G> eval_approx = pack.evaluate(grid);
+		real_t error = 0;
+		for(unsigned i=0; i<G; i++) {
+			real_t diff = std::abs(eval_approx[i]) - std::abs(eval_gold[i]);
+			error += diff*diff;
+		}
+		return std::sqrt(error)/norm_gold;
+	};
 
-	// { // McL42
-	// 	Parameters_Harmonic_1D param_McL42("McL42");
-	// 	propagators::McL42Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> pMcL42(param_McL42.packet,param_McL42.V,param_McL42.splitCoefs);
-	// 	pMcL42.evolve(param_McL42.T,param_McL42.Dt,std::bind(&P::callback,std::ref(param_McL42),_1,_2));
-	// }
 
-	// { // McL84
-	// 	Parameters_Harmonic_1D param_McL84("McL84");
-	// 	propagators::McL84Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> pMcL84(param_McL84.packet,param_McL84.V,param_McL84.splitCoefs);
-	// 	pMcL84.evolve(param_McL84.T,param_McL84.Dt,std::bind(&P::callback,std::ref(param_McL84),_1,_2));
-	// }
+	real_t Dt = .25;
 
-	// { // Pre764
-	// 	Parameters_Harmonic_1D param_Pre764("Pre764");
-	// 	propagators::Pre764Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> pPre764(param_Pre764.packet,param_Pre764.V,param_Pre764.splitCoefs);
-	// 	pPre764.evolve(param_Pre764.T,param_Pre764.Dt,std::bind(&P::callback,std::ref(param_Pre764),_1,_2));
-	// }
+	while(Dt>=2*Dt_gold) {
 
+		{ // Hagedorn
+			Parameters_Harmonic_1D param_Hagedorn;
+			propagators::HagedornPropagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t> propagator_Hagedorn(param_Hagedorn.packet,param_Hagedorn.V);
+			propagator_Hagedorn.evolve(param_Hagedorn.T,Dt);
+			std::cout << "\nHagedorn - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_Hagedorn.packet) << "\n";
+		}
+		
+		{ // Semiclassical
+			Parameters_Harmonic_1D param_Semiclassical;
+			propagators::SemiclassicalPropagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> propagator_Semiclassical(param_Semiclassical.packet,param_Semiclassical.V,param_Semiclassical.splitCoefs);
+			propagator_Semiclassical.evolve(param_Semiclassical.T,Dt);
+			std::cout << "\nSemiclassical - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_Semiclassical.packet) << "\n";
+		}
+
+		{ // MG4
+			Parameters_Harmonic_1D param_MG4;
+			propagators::MG4Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> propagator_MG4(param_MG4.packet,param_MG4.V,param_MG4.splitCoefs);
+			propagator_MG4.evolve(param_MG4.T,Dt);
+			std::cout << "\nMG4 - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_MG4.packet) << "\n";
+		}
+
+		{ // McL42
+			Parameters_Harmonic_1D param_McL42;
+			propagators::McL42Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> propagator_McL42(param_McL42.packet,param_McL42.V,param_McL42.splitCoefs);
+			propagator_McL42.evolve(param_McL42.T,Dt);
+			std::cout << "\nMcL42 - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_McL42.packet) << "\n";
+		}
+
+		{ // McL84
+			Parameters_Harmonic_1D param_McL84;
+			propagators::McL84Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> propagator_McL84(param_McL84.packet,param_McL84.V,param_McL84.splitCoefs);
+			propagator_McL84.evolve(param_McL84.T,Dt);
+			std::cout << "\nMcL84 - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_McL84.packet) << "\n";
+		}
+
+		{ // Pre764
+			Parameters_Harmonic_1D param_Pre764;
+			propagators::Pre764Propagator<P::N,P::D,P::MultiIndex,P::QR,P::Potential,P::Packet_t,P::SplitCoefs_t> propagator_Pre764(param_Pre764.packet,param_Pre764.V,param_Pre764.splitCoefs);
+			propagator_Pre764.evolve(param_Pre764.T,Dt);
+			std::cout << "\nPre764 - Dt: " << std::fixed << Dt << "\tError: " << std::scientific << errorL2(param_Pre764.packet) << "\n";
+		}
+
+		Dt /= 2;
+
+	}
 
     return 0;
 }
